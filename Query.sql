8a4ea17298c99ac2c0f8b32e9158ea34abbebf6e
riinojassoo <riin.ojassoo@hotmail.com>
2024-02-26 13:40:55 +0200

    SQL esimene commit

---
+++ Query.sql
@@ -0,0 +1,24 @@
+--loome db (aktiveeri F5-ga - peab ka enne rea selectima)
+create database TARge23SQL
+
+--db valimine
+use TARge23SQL
+
+--db kustutamine
+drop database TARge23SQL
+
+--tabeli loomine
+create table Gender
+(
+Id int not null primary key,
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine
+insert into Gender (Id, Gender)
+values (1, 'Male'),
+(2, 'Female'),
+(3, 'Unknown')
+
+--vaatame tabeli sisu
+select * from Gender
